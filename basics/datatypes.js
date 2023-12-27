930df6216553a6b5c8ab1886f5a3dce049a2c9a1
04Priyanshu <95174973+04Priyanshu@users.noreply.github.com>
2023-12-27 16:53:43 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,14 @@
+"use strict"//treat all js code in new mode
+
+//alert(3+3) we r using node js not the browser
+
+let name="priyanshu" //string datatype
+let num=18 //number datatype
+let tell=true //boolean
+
+//null= standalone value
+//undefined=vlaue not assinged
+//symbol=uniqueness
+
+
+ 
